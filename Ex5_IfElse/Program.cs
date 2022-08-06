bd6053b3d281021772f267a955b68f676166329a
Andrey <this.is.ya8@gmail.com>
2022-08-06 17:53:54 +1000

    Ex5 If Else

---
+++ Ex5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Даниил")
+{
+    Console.WriteLine("ohayo, oni-chan!");
+}
+else
+{
+    Console.Write("Привет, человек с именем ");
+    Console.WriteLine(username);
+}
